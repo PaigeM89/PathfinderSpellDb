5540ba1321e561a00158bfee5062d9ae215a58c9
Max Paige <Paige.M89@gmail.com>
2023-05-31 10:02:35 -0400

    Delete old svelte project

--- src/Frontend/src/routes/spells/[slug]/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import type { PageData } from './$types';
-  import Spell from '../Spell.svelte';
-
-  export let data: PageData;
-  let spell = data.spell;
-</script>
-
-<Spell spell={spell} />
