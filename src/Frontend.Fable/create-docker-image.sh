c973d7a40ae03cdae8299dda33e268836cb54cde
Max Paige <paige.m89@gmail.com>
2024-02-17 16:36:17 -0500

    Remove deprecated elmish project

--- src/Frontend.Fable/create-docker-image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-yarn tailwindProd
-yarn prod
-
-# "webpack: command not found" ??
-# dotnet fable src -c Release && webpack --mode production --env version=$1
-
-docker build -t pfsdb-client -f Dockerfile-release .
